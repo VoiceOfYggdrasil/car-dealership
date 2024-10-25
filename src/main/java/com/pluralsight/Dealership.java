51ebaf7bfffcff4fbb2a52f3ef59b6c3acb080ee
VoiceOfYggdrasil <aaron.schmitt8@gmail.com>
2024-10-25 11:43:33 -0400

    Returned Null to search methods / Created empty DealershipFileManager

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -51,31 +51,31 @@ private ArrayList<Vehicle> inventory;
     }
 
     public List<Vehicle> getVehiclesByPrice(double min, double max) {
-
+        return null;
     }
 
     public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
-
+        return null;
     }
 
     public List<Vehicle> getVehiclesByYear(int min, int max) {
-
+        return null;
     }
 
     public List<Vehicle> getVehiclesByColor(String color) {
-
+        return null;
     }
 
     public List<Vehicle> getVehiclesByMileage(double min, double max) {
-
+        return null;
     }
 
     public List<Vehicle> getVehiclesByType(String vehicleType) {
-
+        return null;
     }
 
     public List<Vehicle> getAllVehicles() {
-
+        return null;
     }
 
     public void addVehicle(Vehicle vehicle) {
