76aa4e6fa6e6e5b2bab8af43ae49880666158274
Aaron Schmitt <aaron.schmitt8@gmail.com>
2024-10-29 15:50:14 -0400

    Filled Out Dealership Class

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -14,7 +14,7 @@ private ArrayList<Vehicle> inventory;
         this.name = name;
         this.address = address;
         this.phone = phone;
-        inventory = new ArrayList<>();
+        this.inventory = new ArrayList<>();
     }
 
     public String getName() {
@@ -41,48 +41,94 @@ private ArrayList<Vehicle> inventory;
         this.phone = phone;
     }
 
-    @Override
-    public String toString() {
-        return "Dealership{" +
-                "name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", phone='" + phone + '\'' +
-                '}';
-    }
+    public List<Vehicle> getVehiclesByPrice(Double min, Double max) {
+        List<Vehicle> vehicles = new ArrayList<>();
 
-    public List<Vehicle> getVehiclesByPrice(double min, double max) {
-        return null;
+        for (Vehicle vehicle : inventory) {
+            if ((min == null || vehicle.getPrice() >= min) && (max == null || vehicle.getPrice() <= max)) {
+                vehicles.add(vehicle);
+            }
+        }
+        return vehicles;
     }
 
     public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
-        return null;
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        for (Vehicle vehicle : inventory) {
+            if ((vehicle.getMake().equalsIgnoreCase(make) && vehicle.getModel().equalsIgnoreCase(model)) ||
+                    (vehicle.getMake().equalsIgnoreCase(make) && model.isEmpty())) {
+                vehicles.add(vehicle);
+            }
+        }
+        return vehicles;
     }
 
-    public List<Vehicle> getVehiclesByYear(int min, int max) {
-        return null;
+    public List<Vehicle> getVehiclesByYear(Integer min, Integer max) {
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        for(Vehicle vehicle : inventory){
+            if((max == null || vehicle.getYear() <= max) && (min == null || vehicle.getYear() >= min)){
+                vehicles.add(vehicle);
+            }
+        }
+        return vehicles;
     }
 
     public List<Vehicle> getVehiclesByColor(String color) {
-        return null;
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getColor().equalsIgnoreCase(color)) {
+                vehicles.add(vehicle);
+            }
+        }
+        return vehicles;
     }
 
-    public List<Vehicle> getVehiclesByMileage(double min, double max) {
-        return null;
+    public List<Vehicle> getVehiclesByMileage(Double min, Double max) {
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        for (Vehicle vehicle : inventory) {
+            if ((max == null || vehicle.getOdometer() <= max) && (min == null || vehicle.getOdometer() >= min)) {
+                vehicles.add(vehicle);
+            }
+        }
+        return vehicles;
     }
 
     public List<Vehicle> getVehiclesByType(String vehicleType) {
-        return null;
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getVehicleType().equalsIgnoreCase(vehicleType)) {
+                vehicles.add(vehicle);
+            }
+        }
+        return vehicles;
     }
 
     public List<Vehicle> getAllVehicles() {
-        return null;
+        return inventory;
     }
 
     public void addVehicle(Vehicle vehicle) {
-
+        inventory.add(vehicle);
     }
 
     public void removeVehicle(Vehicle vehicle) {
+        inventory.remove(vehicle);
+    }
 
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        output.append(String.format("%s|%s|%s",name,address,phone));
+        for(Vehicle vehicle : inventory){
+            output.append(String.format("%n%d|%d|%s|%s|%s|%s|%d|%.2f",
+                    vehicle.getVin(),vehicle.getYear(),vehicle.getMake(),vehicle.getModel(),vehicle.getVehicleType(),
+                    vehicle.getColor(),vehicle.getOdometer(),vehicle.getPrice()));
+        }
+        return output.toString();
     }
 }
